bd0c6d6d6549012d949ff06cf63272f468286821
Melinda Luo <105473335+mluo123@users.noreply.github.com>
2024-12-06 16:55:35 -0800

    Update download_data.sh

--- install_scripts/download_data.sh
+++ install_scripts/download_data.sh
@@ -13,3 +13,7 @@ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/343/785/GCF_001343785.1_Vi
 # Download H3N2 infleunza assembly and annotation
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/865/085/GCF_000865085.1_ViralMultiSegProj15622/GCF_000865085.1_ViralMultiSegProj15622_genomic.fna.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/865/085/GCF_000865085.1_ViralMultiSegProj15622/GCF_000865085.1_ViralMultiSegProj15622_genomic.gff.gz
+
+# Download hg38 human genome assembly and annotation
+wget https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
+wget https://ftp.ensembl.org/pub/release-110/gff3/homo_sapiens/Homo_sapiens.GRCh38.110.chr.gff3.gz
