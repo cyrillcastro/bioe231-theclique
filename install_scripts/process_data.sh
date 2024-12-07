0a0bc90cb66b5fbc629b7d8ce0d504927bd95a95
Melinda Luo <105473335+mluo123@users.noreply.github.com>
2024-12-06 19:19:39 -0800

    Update process_data.sh

--- install_scripts/process_data.sh
+++ install_scripts/process_data.sh
@@ -43,5 +43,4 @@ bgzip genes.gff
 tabix genes.gff.gz
 
 # Filter BED file for GWAS viewing (NOTE: if your bed file is located in a different location, change the path as needed)
-bgzip ../data/influenzaGWAS.bed
 tabix -0 -b 2 -s 1 -e 2 -f ../data/influenzaGWAS.bed.gz
