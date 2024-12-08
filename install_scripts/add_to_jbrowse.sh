cf0f68472f751f801df541e8d46a5d948c7c4d4e
Melinda Luo <105473335+mluo123@users.noreply.github.com>
2024-12-07 23:02:37 -0800

    Update add_to_jbrowse.sh

--- install_scripts/add_to_jbrowse.sh
+++ install_scripts/add_to_jbrowse.sh
@@ -18,12 +18,12 @@ jbrowse add-track H3N2_genes.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy --as
 jbrowse add-assembly hg38.fa --out $APACHE_ROOT/jbrowse2 --load copy
 jbrowse add-track hg38_genes.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames hg38
 
+# Add GWAS track (change path of zipped bed file as necessary)
+jbrowse add-track ../data/filt_influenzaGWAS.bed.gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames hg38
+
 jbrowse text-index --out $APACHE_ROOT/jbrowse2
 
-# Add connections
+# Add UCSC influenza connections
 jbrowse add-connection https://hgdownload.soe.ucsc.edu/hubs/GCF/000/865/725/GCF_000865725.1/hub.txt --assemblyNames GCF_000865725.1 --out $APACHE_ROOT/jbrowse2 --name H1N1_1934
 jbrowse add-connection https://hgdownload.soe.ucsc.edu/hubs/GCF/001/343/785/GCF_001343785.1/hub.txt --assemblyNames GCF_001343785.1 --out $APACHE_ROOT/jbrowse2 --name H1N1_2009
 jbrowse add-connection https://hgdownload.soe.ucsc.edu/hubs/GCF/000/865/085/GCF_000865085.1/hub.txt --assemblyNames GCF_000865085.1 --out $APACHE_ROOT/jbrowse2 --name H3N2
-
-# Add GWAS track (change path of zipped bed file as necessary)
-jbrowse add-track ../data/filt_influenzaGWAS.bed.gz --out /var/www/html/jbrowse2/ --load copy --assemblyNames hg38
